3fbcf9b0355db9c21f170c1a27bc7a447e027e92
Patrick Steele <prs233@cornell.edu>
2015-09-01 13:56:34 -0400

    Cleaned up the source.
    I changed over to using AMPL as the primary mode name. I expect more
    people will search for AMPL support and not MathProg support, or rather
    than while people who use MathProg probably know of AMPL the reverse
    might not be true.

--- mathprog-mode.el
+++ mathprog-mode.el
@@ -1,20 +1,20 @@
-(defvar mathprog-tab-width 4 "Width of a tab in MATHPROG mode")
+(defvar ampl-tab-width 4 "Width of a tab in AMPL mode")
 
-(defvar mathprog-mode-hook nil)
-(defvar mathprog-basic-offset)
+(defvar ampl-mode-hook nil)
+(defvar ampl-basic-offset)
 
-(defvar mathprog-mode-map
-  (let ((mathprog-mode-map (make-keymap)))
-    (define-key mathprog-mode-map "\C-j" 'newline-and-indent)
-    mathprog-mode-map)
+(defvar ampl-mode-map
+  (let ((ampl-mode-map (make-keymap)))
+    (define-key ampl-mode-map "\C-j" 'newline-and-indent)
+    ampl-mode-map)
   "Keymap for Mathprog/AMPL mode")
 
-(add-to-list 'auto-mode-alist '("\\.mod'" . mathprog-mode))
-(add-to-list 'auto-mode-alist '("\\.dat'" . mathprog-mode))
+(add-to-list 'auto-mode-alist '("\\.mod'" . ampl-mode))
+(add-to-list 'auto-mode-alist '("\\.dat'" . ampl-mode))
 
 ;;; font-lock
 
-(defconst mathprog-declarations
+(defconst ampl-declarations
   '("maximize"
     "minimize"
     "var"
@@ -23,7 +23,7 @@
     "subject to")
   "Keywords that can precede a named declaration.")
 
-(defconst mathprog-reserved-words
+(defconst ampl-reserved-words
   '("Current"
     "IN"
     "INOUT"
@@ -72,7 +72,7 @@
   "The list of reserved words in MathProg/AMPL.")
 
 ;; A list of built-in arithmetic functions
-(defconst mathprog-arithmetic-functions
+(defconst ampl-arithmetic-functions
   '(
     "abs"
     "acos"
@@ -106,36 +106,36 @@
 
 ;; We create optimized regular expressions for the reserved words and
 ;; arithmetic expressions.
-(setq mathprog-declarations-regexp
-      (regexp-opt mathprog-declarations 'words))
-(setq mathprog-reserved-words-regexp
-      (regexp-opt mathprog-reserved-words 'words))
-(setq mathprog-arithmetic-regexp
-      (regexp-opt mathprog-arithmetic-functions 'words))
+(setq ampl-declarations-regexp
+      (regexp-opt ampl-declarations 'words))
+(setq ampl-reserved-words-regexp
+      (regexp-opt ampl-reserved-words 'words))
+(setq ampl-arithmetic-regexp
+      (regexp-opt ampl-arithmetic-functions 'words))
 
-(setq mathprog-name-regexp
+(setq ampl-name-regexp
       "\\<[a-zA-Z_][a-zA-Z_0-9]*")
 
-(defconst mathprog-font-lock-keywords-1
+(defconst ampl-font-lock-keywords-1
   `(
-    (,mathprog-reserved-words-regexp . font-lock-builtin-face)
-    (,mathprog-arithmetic-regexp . font-lock-function-name-face))
+    (,ampl-reserved-words-regexp . font-lock-builtin-face)
+    (,ampl-arithmetic-regexp . font-lock-function-name-face))
   "Handles basic keyword highlighting")
 
-(defun mathprog-declaration-pre-match ()
+(defun ampl-declaration-pre-match ()
   "Search until the end of a match for a name declaration"
   (save-excursion
-    (search-forward-regexp mathprog-name-regexp)))
+    (search-forward-regexp ampl-name-regexp)))
 
-(defun mathprog-test ()
+(defun ampl-test ()
   (save-excursion (search-forward-regexp "\\(variable,\\)*variable'")))
 
-(defconst mathprog-name-matcher 
-  `(,mathprog-declarations-regexp
+(defconst ampl-name-matcher 
+  `(,ampl-declarations-regexp
     (0 font-lock-keyword-face t) 
-    (,mathprog-name-regexp 
+    (,ampl-name-regexp 
      (save-excursion
-       (search-forward-regexp mathprog-name-regexp))
+       (search-forward-regexp ampl-name-regexp))
      nil 
      (0 font-lock-variable-name-face t)))
   "Matches a name declaration.
@@ -153,38 +153,24 @@ For example,
 
 ")
 
-(defconst mathprog-font-lock-keywords-2
+(defconst ampl-font-lock-keywords-2
   (append 
-   mathprog-font-lock-keywords-1
+   ampl-font-lock-keywords-1
    (list
-    mathprog-name-matcher)))
+    ampl-name-matcher)))
 
-(defconst mathprog-font-lock-keywords mathprog-font-lock-keywords-2)
+(defconst ampl-font-lock-keywords ampl-font-lock-keywords-2)
 
 ;;; Indentation
 
-;; We use the following rules for indentation.
+;; For now we use the following simple rules for indentation.
 ;;
-;; 1. If we are at the beginning of the buffer, indent to column 0.
-;; 2. If we are inside a set, param, var, objective, or constraint
-;;    definition, indent by 2 relative to the parent line
-;; 3. If we are inside a brace block, indent to the start of the brace
-;;    block
-;;
+;; 1. The beginning of the buffer is indented to zero.
+;; 2. Lines following a line ending in a colon are indented relative
+;;    to that line.
+;; 3. Lines following a semicolon are not indented.
 
-;; We define a block as one of the following.
-;;
-;; 1. A full expression, optionally terminated by a semicolon
-;; 2. A set of expressions inside a pair of curly braces
-;; 3. Anything inside a pair of square braces
-;;
-;; We indent as follows:
-;;
-;; 1. At the beginning of the buffer we indent to 0.
-;; 2. If we are on the second line of a block, we indent relative to
-;;   the block.
-
-(defun mathprog-indent-line ()
+(defun ampl-indent-line ()
   "Indent the current line"
   (interactive)
   (let ((indentation-level 0))
@@ -194,7 +180,7 @@ For example,
       (setq indentation-level (current-indentation))
       (if (looking-back ":[ ]*$")
           (setq indentation-level
-                (+ mathprog-basic-offset indentation-level)))
+                (+ ampl-basic-offset indentation-level)))
       (if (looking-back ";[ ]*$")
           (setq indentation-level 0)))
     (save-excursion
@@ -204,34 +190,26 @@ For example,
 
 ;;; syntax table
 
-(defvar mathprog-mode-syntax-table
+(defvar ampl-mode-syntax-table
   (let ((st (make-syntax-table)))
-    (modify-syntax-entry ?_ "w" st)     ; Underscores can be in words
-
-    (modify-syntax-entry ?/ ". 14" st)
-    (modify-syntax-entry ?* ". 23" st)
-    (modify-syntax-entry ?# "< b" st)     ; start single-line comments
-    (modify-syntax-entry ?\n "> b" st)
-
-    ;; (modify-syntax-entry ?/ ". 124b" st)  ; / begins and ends /*
-    ;;                                     ; */-style comments
-    ;; (modify-syntax-entry ?\n "> b" st)    ; end single-line comments
-    ;; (modify-syntax-entry ?* ". 23" st) ; * is the second and first
-    ;;                                     ; character in /* */-style
-    ;;                                     ; comment delimters
-    (modify-syntax-entry ?' "\"" st)    ; single-quoted strings
+    (modify-syntax-entry ?_ "w" st)    ; Underscores can be in words
+    (modify-syntax-entry ?/ ". 14" st) ; Multi-line comments (the / char)
+    (modify-syntax-entry ?* ". 23" st) ; Mult-line comments (the * char)
+    (modify-syntax-entry ?# "< b" st)  ; Start single-line comments
+    (modify-syntax-entry ?\n "> b" st) ; End single-line comments
+    (modify-syntax-entry ?' "\"" st)   ; Single-quoted strings
     st)
-  "Syntax table for `mathprog-mode'.")
+  "Syntax table for `ampl-mode'.")
 
-(define-derived-mode mathprog-mode prog-mode "MathProg"
+(define-derived-mode ampl-mode prog-mode "Ampl"
   "Major mode for editing GNU MathProg or AMPL code."
-  (set-syntax-table mathprog-mode-syntax-table)
-  (set (make-local-variable 'font-lock-defaults) '((mathprog-font-lock-keywords)))
-  (set (make-local-variable 'mathprog-basic-offset) 4)
-  (set (make-local-variable 'indent-line-function) 'mathprog-indent-line)
+  (set-syntax-table ampl-mode-syntax-table)
+  (set (make-local-variable 'font-lock-defaults) '((ampl-font-lock-keywords)))
+  (set (make-local-variable 'ampl-basic-offset) 4)
+  (set (make-local-variable 'indent-line-function) 'ampl-indent-line)
   ;(set (make-local-variable 'comment-start) "# "))
   )
 
-(defalias 'ampl-mode 'mathprog-mode)
+(defalias 'mathprog-mode 'ampl-mode)
 (provide 'ampl-mode)
 (provide 'mathprog-mode)
